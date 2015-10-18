c3b71c750932c26d3ad1d6a5bcadc86c96351fb4
Steven <qzs21@qq.com>
2015-10-18 15:50:03 +0800

    初步布局界面

---
+++ code/TCP-UDP-Debuger/TUSessionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TUSessionViewController.swift
+//  TCP-UDP-Debuger
+//
+//  Created by Steven on 15/10/17.
+//  Copyright © 2015年 Neva. All rights reserved.
+//
+
+import UIKit
+
+class TUSessionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        return cell
+    }
+
+}
